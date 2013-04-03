8008394fa0df5792b2e35bf62852087e2376f8c1
Rob Christie <robchristie@gmail.com>
2013-04-03 01:22:41 -0600

    Updated yasnippet to use the elpa version. Made modifications to get snippets working again.

--- emacs.d/rwc-tabs.el
+++ emacs.d/rwc-tabs.el
@@ -2,7 +2,7 @@
 (setq-default indent-tabs-mode nil)
 
 ;; If there is a tab, make it the size of 2 spaces
-;; (setq-default tab-width 2)
+(setq-default tab-width 2)
 
 ;; Mode specific indent sizes
 ;; TODO: Consider putting these in their own mode specific inits
@@ -30,7 +30,7 @@
 ;; Hippie expand.  Groovy vans with tie-dyes.
 
 (setq hippie-expand-try-functions-list
-      '(yas/hippie-try-expand
+      '(yas-hippie-try-expand
         try-expand-dabbrev
         try-expand-dabbrev-all-buffers
         try-expand-dabbrev-from-kill
--- emacs.d/rwc-yasnippet.el
+++ emacs.d/rwc-yasnippet.el
@@ -1,33 +1,31 @@
-;; (add-to-list 'load-path "~/.emacs.d/yasnippet")
-;; (add-to-list 'load-path "~/dev/yasnippet")
-(add-to-list 'load-path "~/dev/vendor/yasnippet-read-only")
 (require 'yasnippet)
 (require 'dropdown-list)
-(setq yas/prompt-functions '( yas/dropdown-prompt
-                              yas/ido-prompt
-                              yas/completing-prompt))
-(yas/initialize)
+(setq yas-prompt-functions '( yas-dropdown-prompt
+                              yas-ido-prompt
+                              yas-completing-prompt))
+;; (yas--initialize)
+(yas-global-mode 1)
 
-;; (yas/load-directory "~/.emacs.d/yasnippet/snippets")
-;; (yas/load-directory "~/.emacs.d/yasnippet/extras/imported")
-(yas/load-directory "~/dev/yasnippet/extras/imported/LaTeX-mode")
-(yas/load-directory "~/dev/yasnippet/extras/imported/c-mode")
-(yas/load-directory "~/dev/yasnippet/extras/imported/css-mode")
-(yas/load-directory "~/dev/yasnippet/extras/imported/html-mode")
-(yas/load-directory "~/dev/yasnippet/extras/imported/lisp-mode")
-(yas/load-directory "~/dev/yasnippet/extras/imported/markdown-mode")
-(yas/load-directory "~/dev/yasnippet/extras/imported/shell-script-mode")
-(yas/load-directory "~/dev/yasnippet/extras/imported/text-mode")
-(yas/load-directory "~/dev/yasnippet/extras/imported/yaml-mode")
-(yas/load-directory "~/dev/yasnippet/snippets/snippet-mode") 
+;; (yas-load-directory "~/.emacs.d/yasnippet/snippets")
+;; (yas-load-directory "~/.emacs.d/yasnippet/extras/imported")
+(yas-load-directory "~/dev/yasnippet/extras/imported/LaTeX-mode")
+(yas-load-directory "~/dev/yasnippet/extras/imported/c-mode")
+(yas-load-directory "~/dev/yasnippet/extras/imported/css-mode")
+(yas-load-directory "~/dev/yasnippet/extras/imported/html-mode")
+(yas-load-directory "~/dev/yasnippet/extras/imported/lisp-mode")
+(yas-load-directory "~/dev/yasnippet/extras/imported/markdown-mode")
+(yas-load-directory "~/dev/yasnippet/extras/imported/shell-script-mode")
+(yas-load-directory "~/dev/yasnippet/extras/imported/text-mode")
+(yas-load-directory "~/dev/yasnippet/extras/imported/yaml-mode")
+(yas-load-directory "~/dev/yasnippet/snippets/snippet-mode") 
 ;; (load "~/dev/yasnippets-ruby/setup.el")  ;; used by ruby snippets
-;; (yas/load-directory "~/dev/yasnippets-ruby")
+;; (yas-load-directory "~/dev/yasnippets-ruby")
 (load "~/dev/yasnippet/extras/imported/ruby-mode/.yas-setup.el")
-(yas/load-directory "~/dev/yasnippet/extras/imported/ruby-mode")
-;; (yas/load-directory "~/dev/yasnippet/extras/imported/rails-mode")
-(yas/load-directory "~/.emacs.d/yasnippets-shoulda")
-(yas/load-directory "~/.emacs.d/vendor/yasnippets-rspec/rspec-snippets")
-(yas/load-directory "~/dev/yasnippets-rails/rails-mode")
+(yas-load-directory "~/dev/yasnippet/extras/imported/ruby-mode")
+;; (yas-load-directory "~/dev/yasnippet/extras/imported/rails-mode")
+(yas-load-directory "~/.emacs.d/yasnippets-shoulda")
+(yas-load-directory "~/.emacs.d/vendor/yasnippets-rspec/rspec-snippets")
+(yas-load-directory "~/dev/yasnippets-rails/rails-mode")
 (load "~/dev/yasnippets-rails/rails-mode/.yas-setup.el")
 
 (require 'autopair)
@@ -35,16 +33,11 @@
 
 ;; Removed and relying on yasnippet to enable my indent or expand
 ;; Hippie expand.  Groovy vans with tie-dyes.
-;; (add-to-list 'hippie-expand-try-functions-list 'yas/hippie-try-expand)
+;; (add-to-list 'hippie-expand-try-functions-list 'yas-hippie-try-expand)
 ;; Replace yasnippets's TAB
-;; (add-hook 'yas/minor-mode-hook
-;;           (lambda () (define-key yas/minor-mode-map
-;;                        (kbd "TAB") 'indent-or-expand))) ; was yas/expand
-
-;; Replace yasnippets's TAB
-(add-hook 'yas/minor-mode-hook
-          (lambda () (define-key yas/minor-mode-map
-                       (kbd "TAB") 'smart-tab))) ; was yas/expand
+;; (add-hook 'yas-minor-mode-hook
+;;           (lambda () (define-key yas-minor-mode-map
+;;                        (kbd "TAB") 'smart-tab))) ; was yas-expand
 
 
 
@@ -54,33 +47,33 @@
 ;; (let ((map (make-sparse-keymap)))
 ;; (define-key map [(control meta prior)] 'mumamo-backward-chunk)
 ;; (define-key map [(control meta next)] 'mumamo-forward-chunk)
-;; (define-key map [tab] 'yas/expand)
+;; (define-key map [tab] 'yas-expand)
 ;; map))
 ;;      (mumamo-add-multi-keymap 'mumamo-multi-major-mode mumamo-map)))
  
 
 
-(defun yas/advise-indent-function (function-symbol)
-  (eval `(defadvice ,function-symbol (around yas/try-expand-first activate)
-           ,(format
-             "Try to expand a snippet before point, then call `%s' as usual"
-             function-symbol)
-           (let ((yas/fallback-behavior nil))
-             (unless (and (interactive-p)
-                          (yas/expand))
-               ad-do-it)))))
+;; (defun yas-advise-indent-function (function-symbol)
+;;   (eval `(defadvice ,function-symbol (around yas-try-expand-first activate)
+;;            ,(format
+;;              "Try to expand a snippet before point, then call `%s' as usual"
+;;              function-symbol)
+;;            (let ((yas-fallback-behavior nil))
+;;              (unless (and (interactive-p)
+;;                           (yas-expand))
+;;                ad-do-it)))))
 
-(yas/advise-indent-function 'ruby-indent-line)
+;; (yas-advise-indent-function 'ruby-indent-line)
 
-(defvar flymake-is-active-flag nil)
+;; (defvar flymake-is-active-flag nil)
 
-;; (defadvice yas/expand-snippet (before inhibit-flymake-syntax-checking-while-expanding-snippet activate)
+;; (defadvice yas-expand-snippet (before inhibit-flymake-syntax-checking-while-expanding-snippet activate)
 ;;   (setq flymake-is-active-flag
 ;;         (member '(flymake-mode . t) (buffer-local-variables)))
 ;;   (when flymake-is-active-flag
 ;;     (flymake-mode-off)))
 
-;; (add-hook 'yas/after-exit-snippet-hook
+;; (add-hook 'yas-after-exit-snippet-hook
 ;;           '(lambda ()
 ;;              (when flymake-is-active-flag
 ;;                (flymake-mode-on)))) 

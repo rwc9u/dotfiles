3362676a82c4ad411e455e278774569a384d1f0c
Rob Christie <90053@lm-6c4008b4e784.den.ofi.com>
2015-01-20 10:10:17 -0700

    Updated .gitignore

--- emacs.d/lisp/private.el
+++
@@ -1,8 +0,0 @@
-(message "loading private")
-
-(setq github-token "e219e248e5125b5f982f17fe57b2517a")
-
-
- ;; '(facebook-api-key "3b027667838f1913cad5ad9f000e0a07")
- ;; '(facebook-api-secret "9a1159d3f36b1b7da8b79759ae951dac")
- ;; '(facebook-session-info (quote ((request_args . [((value . "cc048e9cc19d6f76822bbabd315f82ba") (key . "sig")) ((value . "3b027667838f1913cad5ad9f000e0a07") (key . "api_key")) ((value . "8948da39199765b84a1a8103082715b1") (key . "auth_token")) ((value . "1914961189331284") (key . "call_id")) ((value . "JSON") (key . "format")) ((value . "auth.getSession") (key . "method")) ((value . "1.0") (key . "v"))]) (error_msg . "Invalid parameter") (error_code . 100))))

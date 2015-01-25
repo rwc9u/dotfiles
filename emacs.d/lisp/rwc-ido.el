ca945548e496101b1230c4ba943dda5b39d43d2e
Rob Christie <robchristie@gmail.com>
2015-01-24 23:01:15 -0700

    Added flx-ido and helm.

--- emacs.d/lisp/rwc-ido.el
+++ emacs.d/lisp/rwc-ido.el
@@ -1,6 +1,12 @@
-(require 'ido)
+(require 'flx-ido)
 (ido-mode t)
-(setq ido-enable-flex-matching t) ; fuzzy matching is a must have
+(ido-everywhere 1)
+(flx-ido-mode 1)
+;; disable ido faces to see flx highlights.
+(setq ido-enable-flex-matching t)
+(setq ido-use-faces nil)
+
+
  
 ;; This tab override shouldn't be necessary given ido's default 
 ;; configuration, but minibuffer-complete otherwise dominates the 

304122f17abeacdbac2bb7feeed2c47928a4603c
Rob Christie <90053@ofiglobal.com>
2015-08-24 09:09:27 -0600

    Updated

---
+++ emacs.d/elpa/inf-mongo-20131216.228/inf-mongo-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "inf-mongo" "20131216.228" "Run a MongoDB shell process in a buffer" 'nil :url "http://github.com/tobiassvn/inf-mongo" :keywords '("databases" "mongodb"))

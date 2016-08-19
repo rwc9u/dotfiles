0bebeadb62d0ad1b23eecf3377775a7712924def
Rob Christie <robchristie@gmail.com>
2016-08-18 21:41:19 -0600

    Updated magit-mode.

--- emacs.d/lisp/rwc-git.el
+++ emacs.d/lisp/rwc-git.el
@@ -11,20 +11,8 @@
              (setq global-hl-line-mode nil)
              ))
 
-;; paste to gist
-;; (require 'gist)
-
-;;(if (eq background 'dark)
-(set-face-background 'magit-item-highlight "gray15")
-;;    (set-face-background 'magit-item-highlight "gray95")
-(define-key magit-mode-map (kbd "TAB") 'magit-toggle-section) ; undo tab completion everywhere
-(define-key magit-mode-map [tab] 'magit-toggle-section)       ; undo tab completion everywhere
-
 (eval-after-load 'magit
   '(progn
-     (set-face-foreground 'magit-diff-add "green3")
-     (set-face-foreground 'magit-diff-del "red3")
-     (set-face-background 'magit-item-highlight "gray15")
      (define-key magit-mode-map (kbd "M-3") 'split-window-horizontally) ; was magit-show-level-3
      (define-key magit-mode-map (kbd "M-2") 'split-window-vertically)   ; was magit-show-level-2
      (define-key magit-mode-map (kbd "M-1") 'delete-other-windows)      ; was magit-show-level-1
@@ -34,4 +22,4 @@
      ))
 
 
-(set-variable 'magit-emacsclient-executable "/usr/local/Cellar/emacs/HEAD/bin/emacsclient")
+(set-variable 'magit-emacsclient-executable "/usr/local/Cellar/emacs-mac/emacs-24.5-z-mac-5.13/bin/emacsclient")

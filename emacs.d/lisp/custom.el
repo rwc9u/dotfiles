61eacbd5edc6b1e2d42e1c33ec7fb4e1dd938402
Rob Christie <robchristie@gmail.com>
2020-09-25 11:30:47 -0600

    Minor config updates.

--- emacs.d/lisp/custom.el
+++ emacs.d/lisp/custom.el
@@ -3,23 +3,21 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(backup-directory-alist (quote (("." . "~/backups"))))
+ '(backup-directory-alist '(("." . "~/backups")))
  '(bm-recenter t)
  '(case-fold-search t)
- '(comint-move-point-for-output (quote all))
+ '(comint-move-point-for-output 'all)
  '(compilation-scroll-output t)
  '(compilation-skip-threshold 2)
- '(custom-enabled-themes (quote (sanityinc-solarized-dark)))
+ '(custom-enabled-themes '(sanityinc-solarized-dark))
  '(custom-safe-themes
-   (quote
-    ("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default)))
- '(ediff-split-window-function (quote split-window-horizontally))
+   '("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default))
+ '(ediff-split-window-function 'split-window-horizontally)
  '(fill-column 120)
  '(flx-ido-threshold 100)
  '(flycheck-eslintrc nil)
  '(flymake-allowed-file-name-masks
-   (quote
-    (("\\.c\\'" flymake-simple-make-init)
+   '(("\\.c\\'" flymake-simple-make-init)
      ("\\.cpp\\'" flymake-simple-make-init)
      ("\\.xml\\'" flymake-xml-init)
      ("\\.html?\\'" flymake-xml-init)
@@ -29,10 +27,9 @@
      ("\\.java\\'" jde-ecj-server-flymake-init jde-ecj-flymake-cleanup)
      ("[0-9]+\\.tex\\'" flymake-master-tex-init flymake-master-cleanup)
      ("\\.tex\\'" flymake-simple-tex-init)
-     ("\\.idl\\'" flymake-simple-make-init))))
+     ("\\.idl\\'" flymake-simple-make-init)))
  '(flymake-proc-allowed-file-name-masks
-   (quote
-    (("\\.c\\'" flymake-simple-make-init)
+   '(("\\.c\\'" flymake-simple-make-init)
      ("\\.cpp\\'" flymake-simple-make-init)
      ("\\.xml\\'" flymake-xml-init)
      ("\\.html?\\'" flymake-xml-init)
@@ -42,16 +39,14 @@
      ("\\.java\\'" jde-ecj-server-flymake-init jde-ecj-flymake-cleanup)
      ("[0-9]+\\.tex\\'" flymake-master-tex-init flymake-master-cleanup)
      ("\\.tex\\'" flymake-simple-tex-init)
-     ("\\.idl\\'" flymake-simple-make-init))))
+     ("\\.idl\\'" flymake-simple-make-init)))
  '(global-font-lock-mode t nil (font-lock))
  '(grep-find-command
    "find . -path '*/.svn/*' -or -path '*/build/*' -or -path '*/.git/*' -prune -o -type f -print0 | xargs -0 grep -nH -e ")
  '(grep-find-ignored-directories
-   (quote
-    ("SCCS" "RCS" "CVS" "MCVS" ".svn" ".git" ".hg" ".bzr" "_MTN" "_darcs" "{arch}" "coverage" "logs")))
+   '("SCCS" "RCS" "CVS" "MCVS" ".svn" ".git" ".hg" ".bzr" "_MTN" "_darcs" "{arch}" "coverage" "logs"))
  '(hippie-expand-try-functions-list
-   (quote
-    (try-complete-file-name-partially try-complete-file-name try-expand-all-abbrevs try-expand-list try-expand-dabbrev try-expand-dabbrev-all-buffers try-expand-dabbrev-from-kill try-complete-lisp-symbol-partially try-complete-lisp-symbol try-expand-line)))
+   '(try-complete-file-name-partially try-complete-file-name try-expand-all-abbrevs try-expand-list try-expand-dabbrev try-expand-dabbrev-all-buffers try-expand-dabbrev-from-kill try-complete-lisp-symbol-partially try-complete-lisp-symbol try-expand-line))
  '(isearch-allow-scroll t)
  '(iswitchb-max-to-show 40)
  '(js-indent-level 2)
@@ -61,9 +56,11 @@
    "https://gist.github.com/jasonm23/516763/raw/632b066c7c18edc91485ba13b37ba50a9b2caac7/screen.css")
  '(max-lisp-eval-depth 1024)
  '(max-specpdl-size 15000)
+ '(package-archives
+   '(("gnu" . "http://elpa.gnu.org/packages/")
+     ("melpa" . "https://melpa.org/packages/")))
  '(package-selected-packages
-   (quote
-    (dockerfile-mode docker-compose-mode projectile-rails groovy-mode js2-refactor powerline color-theme-sanityinc-solarized color-theme fill-column-indicator coffee-mode js2-mode magit))))
+   '(go-mode dockerfile-mode docker-compose-mode projectile-rails groovy-mode js2-refactor powerline color-theme-sanityinc-solarized color-theme fill-column-indicator coffee-mode js2-mode magit)))
 '(ruby-program-name "/opt/local/bin/irb --inf-ruby-mode" t)
 '(scss-compile-at-save nil)
 '(user-full-name "Rob Christie")

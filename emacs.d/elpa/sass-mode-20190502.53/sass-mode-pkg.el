650cebc9ac0e22b555ff45468b61126cf1674acc
Rob Christie <rob.christie@kajabi.com>
2022-10-27 20:35:47 -0600

    Add more packages

---
+++ emacs.d/elpa/sass-mode-20190502.53/sass-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from sass-mode.el  -*- no-byte-compile: t -*-
+(define-package "sass-mode" "20190502.53" "Major mode for editing Sass files" '((haml-mode "3.0.15") (cl-lib "0.5")) :commit "247a0d4b509f10b28e4687cd8763492bca03599b" :authors '(("Natalie Weizenbaum")) :maintainer '("Natalie Weizenbaum") :keywords '("markup" "language" "css") :url "http://github.com/nex3/haml/tree/master")

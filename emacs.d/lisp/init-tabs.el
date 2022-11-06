38f945cc56aae1f57e265b02170d4ff82d5d603a
Rob Christie <rob.christie@kajabi.com>
2022-11-06 00:02:35 -0600

    More cleanup

--- emacs.d/lisp/init-tabs.el
+++ emacs.d/lisp/init-tabs.el
@@ -6,9 +6,6 @@
 
 ;; Mode specific indent sizes
 ;; TODO: Consider putting these in their own mode specific inits
-(setq c-basic-offset 4)
-(setq css-indent-offset 2)
-(setq sh-basic-offset 2)
 (set-default 'javascript-indent-level 2)
 
 ;; (global-set-key (kbd "TAB") 'indent-or-expand)

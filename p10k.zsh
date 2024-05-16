d14d5ea2c31b01929fdcbb55a1d5c5b467a8fdd3
Rob Christie <rob.christie@kajabi.com>
2024-05-15 22:55:27 -0600

    cleanup node version in prompt since I'm using asdf now

--- p10k.zsh
+++ p10k.zsh
@@ -56,9 +56,9 @@
     pyenv                   # python environment (https://github.com/pyenv/pyenv)
     goenv                   # go environment (https://github.com/syndbg/goenv)
     # nodenv                # node.js version from nodenv (https://github.com/nodenv/nodenv)
-    nvm                     # node.js version from nvm (https://github.com/nvm-sh/nvm)
+    # nvm                     # node.js version from nvm (https://github.com/nvm-sh/nvm)
     # nodeenv               # node.js environment (https://github.com/ekalinin/nodeenv)
-    node_version            # node.js version
+    # node_version            # node.js version
     # go_version            # go version (https://golang.org)
     # rust_version          # rustc version (https://www.rust-lang.org)
     # dotnet_version        # .NET version (https://dotnet.microsoft.com)
